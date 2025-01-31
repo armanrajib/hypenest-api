a9bcb40de938712db3a9c68bbac8a0b83aeea65e
Arman Rajib <ahrajib6789@gmail.com>
2025-01-31 19:47:30 +0600

    Implement USER model, routes, and controllers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config({ path: './.env' });
 
@@ -26,5 +27,6 @@ app.use(express.json());
 // =======
 
 app.use('/api/v1/products', productRouter);
+app.use('/api/v1/users', userRouter);
 
 export default app;
---
+++ models/userModel.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please tell us your name!'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same!',
+      },
+    },
+    address: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: [true, 'Please provide your phone number'],
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Encrypt password before saving to database
+// -------------------------------------------
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+// MODEL
+// ------
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
